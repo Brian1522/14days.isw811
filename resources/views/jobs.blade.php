daaabcc75864d5bd8851b21f7a16154655d07370
Brian Laguna <66448870+Brian1522@users.noreply.github.com>
2024-10-26 02:49:43 -0600

    ll-Eloquent_Eager_Loading
    ll-Eloquent_Eager_Loading_and_the_N+1_Problem

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,17 +1,18 @@
-
 <x-layout>
-<x-slot:heading>
-       Job Listenings
-</x-slot:heading>
-<ul>
-    @foreach ($jobs as $job)
-        <li>
-            <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
+    <x-slot:heading>
+        Job Listenings
+    </x-slot:heading>
+
+    <div class="space-y-4">
+        @foreach ($jobs as $job)
+       
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
+
+                <div>
                 <strong>{{ $job['title'] }}:</strong> Pays {{ $job['Salary'] }} per year.
+                </div>
             </a>
-        </li>
-    @endforeach
-</ul>
+        @endforeach
+    </div>
 </x-layout>
-
-
